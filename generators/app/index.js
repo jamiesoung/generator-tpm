3a6db9434bf771551bcad4f45512e7c781ef1735
婳奕 <yuna.lyn@alibaba-inc.com>
2015-08-19 18:40:43 +0800

    mobile 不用jquery

--- generators/app/index.js
+++ generators/app/index.js
@@ -22,15 +22,7 @@ module.exports = yeoman.generators.Base.extend({
       'It is a generator for mobile. It depend on zepTo or jQuery.'
     ));
 
-    var prompts = [{
-      type: 'list',
-      name: 'features',
-      message: 'Would you like to enable this option?',
-      choices : [{
-
-        name: 'zepTo(m)',
-        value: 'zepto'
-      },
+    /*
       {
 
         name: 'jQuery-2.1.4(pc，不支持ie6-8)',
@@ -40,6 +32,16 @@ module.exports = yeoman.generators.Base.extend({
 
         name: 'jQuery-1.11.3(pc)',
         value: 'jquery1'
+      }
+    */
+    var prompts = [{
+      type: 'list',
+      name: 'features',
+      message: 'Would you like to enable this option?',
+      choices : [{
+
+        name: 'zepTo(m)',
+        value: 'zepto'
       }
       ]
     }];
@@ -64,7 +66,7 @@ module.exports = yeoman.generators.Base.extend({
   writing: {
 
     app: function(){
-      
+
       this.template('_package.json', 'package.json');
       this.template('gulpfile.js', 'gulpfile.js');
       this.template('_gitignore', '.gitignore');
@@ -80,16 +82,16 @@ module.exports = yeoman.generators.Base.extend({
       this.copy('src/p/index/index.less', 'src/p/index/index.less');
       this.copy('README.md', 'README.md');
 
-      if(this.isZepto) {
-        this.copy('src/c/lib/zepto.js', 'src/c/lib/zepto.js');  
-         this.copy('src/p/index/index_zepto.js', 'src/p/index/index.js');
-      } else if(this.isJquery1) {
-        this.copy('src/c/lib/jquery-1.11.3.min.js', 'src/c/lib/jquery-1.11.3.min.js');
-         this.copy('src/p/index/index_jquery1.js', 'src/p/index/index.js');
-      } else {
-        this.copy('src/c/lib/jquery-2.1.4.min.js', 'src/c/lib/jquery-2.1.4.min.js');
-         this.copy('src/p/index/index_jquery2.js', 'src/p/index/index.js');
-      }
+      // if(this.isZepto) {
+      this.copy('src/c/lib/zepto.js', 'src/c/lib/zepto.js');  
+      this.copy('src/p/index/index_zepto.js', 'src/p/index/index.js');
+      // } else if(this.isJquery1) {
+      //   this.copy('src/c/lib/jquery-1.11.3.min.js', 'src/c/lib/jquery-1.11.3.min.js');
+      //    this.copy('src/p/index/index_jquery1.js', 'src/p/index/index.js');
+      // } else {
+      //   this.copy('src/c/lib/jquery-2.1.4.min.js', 'src/c/lib/jquery-2.1.4.min.js');
+      //    this.copy('src/p/index/index_jquery2.js', 'src/p/index/index.js');
+      // }
 
       this.copy('demo/index.html', 'demo/index.html');
     }
